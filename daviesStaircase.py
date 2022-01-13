71bc7843ade0da7fff150823d659626cd3bd5d3a
codewurld <sholaq@gmail.com>
2022-01-13 20:02:46 +0000

    comments added

---
+++ daviesStaircase.py
@@ -0,0 +1,13 @@
+
+result = dict() #creates empty object
+result[1] = 1
+result[2] = 2
+result[3] = 4 #possible permutation of steps using 1, 2 or 3 steps
+
+
+def stepPerms(n):
+    if n == 0:
+        return 0
+    if n in result.keys(): #if n exists already return the value of n (memoization), else assign new key with n 
+        return result[n] 
+    result[n] = stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3) #next amt of ways = sum of prev 3
