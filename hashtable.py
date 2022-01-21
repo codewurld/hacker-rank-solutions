fd6c5875f891a342e36a6bd58e270dbb63eda6cb
codewurld <sholaq@gmail.com>
2022-01-21 21:39:08 +0000

    cache url hashmap

--- hashtable.py
+++ hashtable.py
@@ -1,4 +1,4 @@
-
+#hash table for voters
 voted = {}
 
 def check_voter(name):
@@ -11,3 +11,15 @@ def check_voter(name):
 check_voter("Shola")
 check_voter("Shola")
 check_voter("Baxton")
+
+#hash table that checks if a url is cached
+# if not server runs and returned data is cached
+cache = {}
+
+def get_page(url):
+    if cache.get(url):
+        return cache(url)
+    else:
+        data = get_data_from_server(url)
+        cache[url] = data
+        return data
