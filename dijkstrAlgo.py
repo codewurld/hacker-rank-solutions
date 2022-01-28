b5639b0b400e4357e3aef8f5c566150c45f3ed35
codewurld <sholaq@gmail.com>
2022-01-28 18:21:19 +0000

    algorithm implementation

--- dijkstrAlgo.py
+++ dijkstrAlgo.py
@@ -1,4 +1,7 @@
 # problem on p.384 Grokking Algorithm
+
+#SET UP
+
 #graph in hashtable
 graph = {}
 # nest "start" graph in original graph 
@@ -14,7 +17,58 @@ graph["a"]["fin"] = 1
 
 #nest "b" graph within original graph 
 graph["b"] = {}
-graph["a"]["fin"] = 1
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
 
+#nest empty "fin" graph within original graph 
+graph["fin"] = {}
+
+
+#cost hashtable
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+#parents hashtable
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+
+
+#find value of edge b
 print(graph['start']["b"])
+#find key values of start graph
+print(graph['start'].keys())
+
+#IMPLEMENTATION
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if lowest_cost > cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+#keep track of processed nodes
+processed = []
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbours = graph[node]
+    for n in neighbours.keys():
+        new_cost = cost + neighbours[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
 
